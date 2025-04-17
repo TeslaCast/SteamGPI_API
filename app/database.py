38a5e3313fb80c5e9bcfa842a312428ba25a8723
TeslaCast <owibka692@gmail.com>
2025-04-17 21:30:10 +0300

    WORKING VERION: SAVE TO REVERT

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-
+ 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/SteamGPI_API")
 
 # Create the engine for connecting to the database without check_same_thread
